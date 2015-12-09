4ffa29d1e2df34fb050b2e98cd91b03aa5eba6b1
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-12-09 19:29:59 -0300

    Updated Ex 3 begin solution

---
+++ HOL/BringingTogetherOneAspNet/Source/Ex3-WebAPI/Begin/src/MyWebApplication/Models/PersonContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Entity;
+
+namespace MyWebApplication.Models
+{
+    public class PersonContext : DbContext
+    {
+        private static bool _created = false;
+
+        public PersonContext()
+        {
+            if (!_created)
+            {
+                _created = true;
+                Database.EnsureCreated();
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
