842acbdf5b227b52f7e8bfca419457922e796cfd
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-12-10 20:03:18 -0300

    Updated Exercise 1 end solution for "ASP.NET MVC 6 and Single-Page Applications (SPAs)" HOL

---
+++ HOL/AspNetApiSpa/Source/Ex1-CreatingAnAPI/End/src/GeekQuiz/Models/TriviaQuestion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekQuiz.Models
+{
+    public class TriviaQuestion
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public virtual List<TriviaOption> Options { get; set; }
+    }
+}
