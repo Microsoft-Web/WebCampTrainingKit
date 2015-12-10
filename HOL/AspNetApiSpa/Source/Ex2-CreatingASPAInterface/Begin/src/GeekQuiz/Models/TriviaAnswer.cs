70b144f2ab7a4afc9824397471d35fbd13dfefeb
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-12-10 20:03:35 -0300

    Updated Exercise 2 begin solution for "ASP.NET MVC 6 and Single-Page Applications (SPAs)" HOL

---
+++ HOL/AspNetApiSpa/Source/Ex2-CreatingASPAInterface/Begin/src/GeekQuiz/Models/TriviaAnswer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace GeekQuiz.Models
+{
+    public class TriviaAnswer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        
+        public int OptionId { get; set; }
+        
+        public int QuestionId { get; set; }
+
+        [JsonIgnore]
+        public virtual TriviaOption TriviaOption { get; set; }
+    }
+}
