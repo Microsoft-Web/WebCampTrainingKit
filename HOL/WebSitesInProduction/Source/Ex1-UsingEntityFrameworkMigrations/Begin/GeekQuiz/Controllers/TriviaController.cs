5fd440c2c54244ff72728a8f0b8fa381c4049475
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-12-15 18:28:08 -0300

    Updated Begin solution for Ex 1

--- HOL/WebSitesInProduction/Source/Ex1-UsingEntityFrameworkMigrations/Begin/GeekQuiz/Controllers/TriviaController.cs
+++ HOL/WebSitesInProduction/Source/Ex1-UsingEntityFrameworkMigrations/Begin/GeekQuiz/Controllers/TriviaController.cs
@@ -1,84 +1,68 @@
-﻿namespace GeekQuiz.Controllers
+using System.Threading.Tasks;
+using GeekQuiz.Models;
+using GeekQuiz.Services;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace GeekQuiz.Controllers
 {
-    using System.Data.Entity;
-    using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using System.Web.Http;
-    using System.Web.Http.Description;
-    using GeekQuiz.Models;
-
+    [Produces("application/json")]
+    [Route("api/Trivia")]
     [Authorize]
-    public class TriviaController : ApiController
+    public class TriviaController : Controller
     {
-        private TriviaContext db = new TriviaContext();
+        private TriviaDbContext context;
+        private IQuestionsService questionsService;
+        private IAnswersService answersService;
 
-        // GET api/Trivia
-        [ResponseType(typeof(TriviaQuestion))]
-        public async Task<IHttpActionResult> Get()
+        public TriviaController(TriviaDbContext context, IQuestionsService questionsService, IAnswersService answersService)
+        {
+            this.context = context;
+            this.questionsService = questionsService;
+            this.answersService = answersService;
+        }
+        
+        // GET: api/Trivia
+        [HttpGet]
+        public async Task<IActionResult> Get()
         {
             var userId = User.Identity.Name;
 
-            TriviaQuestion nextQuestion = await this.NextQuestionAsync(userId);
+            TriviaQuestion nextQuestion =
+                await this.questionsService.NextQuestionAsync(userId);
 
             if (nextQuestion == null)
             {
-                return this.NotFound();
+                return HttpNotFound();
             }
 
-            return this.Ok(nextQuestion);
+            return Ok(nextQuestion);
         }
 
-        // POST api/Trivia
-        [ResponseType(typeof(TriviaAnswer))]
-        public async Task<IHttpActionResult> Post(TriviaAnswer answer)
+        // POST: api/Trivia
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TriviaAnswer answer)
         {
             if (!ModelState.IsValid)
             {
-                return this.BadRequest(this.ModelState);
+                return HttpBadRequest(ModelState);
             }
 
             answer.UserId = User.Identity.Name;
 
-            var isCorrect = await this.StoreAsync(answer);
-            return this.Ok<bool>(isCorrect);
+            var isCorrect = await this.answersService.StoreAsync(answer);
+            
+            return this.CreatedAtAction("Get", new {}, isCorrect);
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                this.db.Dispose();
+                context.Dispose();
             }
 
             base.Dispose(disposing);
-        }
-
-        private async Task<TriviaQuestion> NextQuestionAsync(string userId)
-        {
-            var lastQuestionId = await this.db.TriviaAnswers
-                .Where(a => a.UserId == userId)
-                .GroupBy(a => a.QuestionId)
-                .Select(g => new { QuestionId = g.Key, Count = g.Count() })
-                .OrderByDescending(q => new { q.Count, QuestionId = q.QuestionId })
-                .Select(q => q.QuestionId)
-                .FirstOrDefaultAsync();
-
-            var questionsCount = await this.db.TriviaQuestions.CountAsync();
-
-            var nextQuestionId = (lastQuestionId % questionsCount) + 1;
-            return await this.db.TriviaQuestions.FindAsync(CancellationToken.None, nextQuestionId);
-        }
-
-        private async Task<bool> StoreAsync(TriviaAnswer answer)
-        {
-            this.db.TriviaAnswers.Add(answer);
-
-            await this.db.SaveChangesAsync();
-            var selectedOption = await this.db.TriviaOptions.FirstOrDefaultAsync(o => o.Id == answer.OptionId
-                && o.QuestionId == answer.QuestionId);
-
-            return selectedOption.IsCorrect;
         }
     }
 }
--- HOL/WebSitesInProduction/Source/Ex1-UsingEntityFrameworkMigrations/Begin/GeekQuiz/Models/TriviaQuestion.cs
+++ HOL/WebSitesInProduction/Source/Ex1-UsingEntityFrameworkMigrations/Begin/GeekQuiz/Models/TriviaQuestion.cs
@@ -1,10 +1,12 @@
-﻿namespace GeekQuiz.Models
-{
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace GeekQuiz.Models
+{
     public class TriviaQuestion
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
