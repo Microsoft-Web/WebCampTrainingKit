7193ef6650df2f5a0448cd5d32d31256389e0c30
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-12-15 19:04:44 -0300

    Updated End solution for Exercise 1

--- HOL/WebSitesInProduction/Source/Ex1-UsingEntityFrameworkMigrations/End/GeekQuiz/Models/TriviaQuestion.cs
+++ HOL/WebSitesInProduction/Source/Ex1-UsingEntityFrameworkMigrations/End/GeekQuiz/Models/TriviaQuestion.cs
@@ -1,18 +1,19 @@
-﻿namespace GeekQuiz.Models
-{
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace GeekQuiz.Models
+{
     public class TriviaQuestion
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
         public string Title { get; set; }
 
         public virtual List<TriviaOption> Options { get; set; }
-
-        [StringLength(150)]
+        
         public string Hint { get; set; }
     }
 }
