8d5f09efb9b375c4f6e5e19806cc248ca16d131a
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-12-17 12:49:28 -0300

    Added begin solution for Ex 4

---
+++ HOL/WebSitesInProduction/Source/Ex4-ScalingUsingWindowsAzureStorage/Begin/GeekQuiz/Services/AnswersService.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using GeekQuiz.Models;
+using Microsoft.Data.Entity;
+
+namespace GeekQuiz.Services
+{
+    public class AnswersService : IAnswersService
+    {
+        private TriviaDbContext db;
+
+        public AnswersService(TriviaDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<bool> StoreAsync(TriviaAnswer answer)
+        {
+            var selectedOption = await this.db.TriviaOptions.FirstOrDefaultAsync(o => 
+                o.Id == answer.OptionId
+                && o.QuestionId == answer.QuestionId);
+
+            if (selectedOption != null)
+            {
+                answer.TriviaOption = selectedOption;
+                this.db.TriviaAnswers.Add(answer);
+
+                await this.db.SaveChangesAsync();
+            }
+
+            return selectedOption.IsCorrect;
+        }
+    }
+}
